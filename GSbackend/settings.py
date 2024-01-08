0534baec6d7aaae72f2ed5c4f58253a6e8e77774
HoverGo <kotikarbuz@gmail.com>
2024-01-08 16:28:08 +0500

    base data

--- GSbackend/settings.py
+++ GSbackend/settings.py
@@ -102,8 +102,12 @@ WSGI_APPLICATION = "GSbackend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "fadbc6*g**d*Gg53e-FAE622FDD-BAFd",
+        "HOST": "viaduct.proxy.rlwy.net",
+        "PORT": "52922",
     }
 }
 
